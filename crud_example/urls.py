99c39a7717830b3016e80179df2ad7b59a07d58c
Dan-UCC <daniel.vegacruz@campusucc.edu.co>
2025-11-20 22:01:07 -0500

    Entrega final - Proyecto completo SGTL + CRUD Vehículos/Reserva

--- crud_example/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('vehicles/', include('vehiclesapp.urls')),
-    path('', lambda request: redirect('list_vehiculos', permanent=False)),  # Ruta raíz: redirige a /vehicles/
-]
